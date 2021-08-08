e5d8446ced243b2a6f0a668405850f7e3faf528f
Dmitry Vorobev <dm.siberian@gmail.com>
2021-08-08 14:59:48 +0300

    converted trip-info component to class TripInfo

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
+import TripInfoView from './view/trip-info';
 import SiteMenuView from './view/site-menu';
 import FilterListView from './view/filters';
 import SortView from './view/sort';
 import EventListView from './view/event-list';
-import {createTripInfoTemplate} from './view/trip-info';
 import {createEditTripPointFormTemplate} from './view/edit-event-form';
 import {createTripPointTemplate} from './view/event';
 import {createTripPointObjects} from './mock/trip-point';
@@ -10,15 +10,15 @@ import {renderElement, renderTemplate, RenderPosition} from './utils';
 
 const TRIP_POINTS_OBJECTS = 15;
 
-const tripPoints = createTripPointObjects(TRIP_POINTS_OBJECTS);
+const events = createTripPointObjects(TRIP_POINTS_OBJECTS);
 
 const siteMainContainer = document.querySelector('.page-body');
+const tripInfoContainer = siteMainContainer.querySelector('.trip-main');
 const siteMenuContainer = siteMainContainer.querySelector('.trip-controls__navigation');
 const filterListContainer = siteMainContainer.querySelector('.trip-controls__filters');
-const tripInfoContainer = siteMainContainer.querySelector('.trip-main');
 const tripEventsContainer = siteMainContainer.querySelector('.trip-events');
 
-renderTemplate(tripInfoContainer, createTripInfoTemplate(tripPoints), 'afterbegin');
+renderElement(tripInfoContainer, new TripInfoView(events).getElement(), RenderPosition.AFTERBEGIN);
 renderElement(siteMenuContainer, new SiteMenuView().getElement(), RenderPosition.BEFOREEND);
 renderElement(filterListContainer, new FilterListView().getElement(), RenderPosition.BEFOREEND);
 renderElement(tripEventsContainer, new SortView().getElement(), RenderPosition.AFTERBEGIN);
@@ -26,11 +26,11 @@ renderElement(tripEventsContainer, new EventListView().getElement(), RenderPosit
 
 
 const eventListContainer = tripEventsContainer.querySelector('.trip-events__list');
-for (let i = 0; i < tripPoints.length; i++) {
+for (let i = 0; i < events.length; i++) {
   if (i === 0) {
-    renderTemplate(eventListContainer, createEditTripPointFormTemplate(tripPoints[i]), 'beforeend');
+    renderTemplate(eventListContainer, createEditTripPointFormTemplate(events[i]), 'beforeend');
   }
   else {
-    renderTemplate(eventListContainer, createTripPointTemplate(tripPoints[i]), 'beforeend');
+    renderTemplate(eventListContainer, createTripPointTemplate(events[i]), 'beforeend');
   }
 }
