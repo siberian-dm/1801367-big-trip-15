9b4cc6fbfae000d7c43953983ae009eebb6c6eb2
Dmitry Vorobev <dm.siberian@gmail.com>
2021-08-13 17:03:24 +0300

    fixed variable names and refactored

--- src/view/trip-point.js
+++ src/view/trip-point.js
@@ -18,14 +18,8 @@ const createSelectedOffersTemplate = (offers) => {
 };
 
 
-const createTripPointTemplate = (event) => {
-  const {basePrice, dateFrom, dateTo, destination, offers, type, isFavorite} = event;
-
-  const favoriteClassName = isFavorite
-    ? 'event__favorite-btn event__favorite-btn--active'
-    : 'event__favorite-btn';
-
-  const eventSelectedOffers = offers.length ? createSelectedOffersTemplate(offers) : '';
+const createTripPointTemplate = (tripPoint) => {
+  const {basePrice, dateFrom, dateTo, destination, offers, type, isFavorite} = tripPoint;
 
   return (
     `<li class="trip-events__item">
@@ -48,9 +42,9 @@ const createTripPointTemplate = (event) => {
         </p>
         <h4 class="visually-hidden">Offers:</h4>
         <ul class="event__selected-offers">
-          ${eventSelectedOffers}
+          ${offers.length ? createSelectedOffersTemplate(offers) : ''}
         </ul>
-        <button class="${favoriteClassName}" type="button">
+        <button class="event__favorite-btn ${isFavorite ? 'event__favorite-btn--active' : ''}" type="button">
           <span class="visually-hidden">Add to favorite</span>
           <svg class="event__favorite-icon" width="28" height="28" viewBox="0 0 28 28">
             <path d="M14 21l-8.22899 4.3262 1.57159-9.1631L.685209 9.67376 9.8855 8.33688 14 0l4.1145 8.33688 9.2003 1.33688-6.6574 6.48934 1.5716 9.1631L14 21z"/>
@@ -66,14 +60,14 @@ const createTripPointTemplate = (event) => {
 
 
 export default class TripPoint extends AbstractView {
-  constructor(event) {
+  constructor(tripPoints) {
     super();
-    this._event = event;
+    this._tripPoints = tripPoints;
     this._switchToFormHandler = this._switchToFormHandler.bind(this);
   }
 
   getTemplate() {
-    return createTripPointTemplate(this._event);
+    return createTripPointTemplate(this._tripPoints);
   }
 
   _switchToFormHandler(evt) {
