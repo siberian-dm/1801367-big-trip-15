0c7d1f255b6d8b5bc7308153b58d01693c9e3a3a
Dmitry Vorobev <dm.siberian@gmail.com>
2021-08-13 17:04:55 +0300

    fixed function name

--- src/view/no-trip-points.js
+++ src/view/no-trip-points.js
@@ -1,10 +1,10 @@
 import AbstractView from './abstract';
 
-const createNoEventsTemplate = () => '<p class="trip-events__msg">Click New Event to create your first point</p>';
+const createNoTripPointsTemplate = () => '<p class="trip-events__msg">Click New Event to create your first point</p>';
 
 
 export default class NoTripPoints extends AbstractView {
   getTemplate() {
-    return createNoEventsTemplate();
+    return createNoTripPointsTemplate();
   }
 }
