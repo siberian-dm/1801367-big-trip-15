bb4baf512cdc32782659f1fa804efe89a393e622
Dmitry Vorobev <dm.siberian@gmail.com>
2021-08-14 17:51:02 +0300

    replaced firstChild with firstElementChild in createElement () function

--- src/utils/render.js
+++ src/utils/render.js
@@ -58,5 +58,5 @@ export const createElement = (template) => {
   const newElement = document.createElement('div');
   newElement.innerHTML = template;
 
-  return newElement.firstChild;
+  return newElement.firstElementChild;
 };
