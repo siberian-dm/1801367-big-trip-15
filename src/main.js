d80d9e206eece9b9d08702a4d713909ef6217ab4
Dmitry Vorobev <dm.siberian@gmail.com>
2021-08-17 15:53:09 +0300

    renamed variables in main.js

--- src/main.js
+++ src/main.js
@@ -3,14 +3,14 @@ import {createTripPointObjects} from './mock/trip-point';
 
 const TRIP_POINT_COUNT = 15;
 
-const tripPoints = createTripPointObjects(TRIP_POINT_COUNT);
+const points = createTripPointObjects(TRIP_POINT_COUNT);
 
-const siteMainContainer = document.querySelector('.page-body');
-const tripInfoContainer = siteMainContainer.querySelector('.trip-main');
-const siteMenuContainer = siteMainContainer.querySelector('.trip-controls__navigation');
-const filtersContainer = siteMainContainer.querySelector('.trip-controls__filters');
-const tripPointsContainer = siteMainContainer.querySelector('.trip-events');
+const mainContainer = document.querySelector('.page-body');
+const infoContainer = mainContainer.querySelector('.trip-main');
+const menuContainer = mainContainer.querySelector('.trip-controls__navigation');
+const filtersContainer = mainContainer.querySelector('.trip-controls__filters');
+const pointsContainer = mainContainer.querySelector('.trip-events');
 
-const tripPresenter = new TripPresenter(tripInfoContainer, siteMenuContainer, filtersContainer, tripPointsContainer);
+const tripPresenter = new TripPresenter(infoContainer, menuContainer, filtersContainer, pointsContainer);
 
-tripPresenter.init(tripPoints);
+tripPresenter.init(points);
