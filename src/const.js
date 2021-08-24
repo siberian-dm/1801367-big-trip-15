807f9d177893716ffa1bf510bc1b52b2bf304096
Dmitry Vorobev <dm.siberian@gmail.com>
2021-08-24 13:27:26 +0300

    Added DESTINATIONS constant

--- src/const.js
+++ src/const.js
@@ -115,7 +115,94 @@ export const OFFER_TYPES = [
     'offers': [],
   },
 ];
-export const CITIES = ['Amsterdam', 'Chamonix', 'Geneva', 'Prague', 'Berlin', 'Stockholm'];
+export const DESTINATIONS = [
+  {
+    description: 'Sed sed nisi sed augue convallis suscipit in sed felis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam id orci ut lectus varius viverra.',
+    name: 'Amsterdam',
+    pictures: [
+      {
+        src: 'http://picsum.photos/248/152?r=4',
+        description: 'Cras aliquet varius magna, non porta ligula feugiat eget. Aliquam id orci ut lectus varius viverra.',
+      },
+    ],
+  },
+  {
+    description: 'Sed sed nisi sed augue convallis suscipit in sed felis. Sed sed nisi sed augue convallis suscipit in sed felis.',
+    name: 'Chamonix',
+    pictures: [
+      {
+        src: 'http://picsum.photos/248/152?r=2',
+        description: 'Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante. Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante. Aliquam erat volutpat. Cras aliquet varius magna, non porta ligula feugiat eget.',
+      },
+      {
+        src: 'http://picsum.photos/248/152?r=2',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sed nisi sed augue convallis suscipit in sed felis. Aliquam erat volutpat.',
+      },
+      {
+        src: 'http://picsum.photos/248/152?r=3',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam id orci ut lectus varius viverra. Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum. Nunc fermentum tortor ac porta dapibus. Aliquam erat volutpat.',
+      },
+      {
+        src: 'http://picsum.photos/248/152?r=3',
+        description: 'Aliquam id orci ut lectus varius viverra.',
+      },
+    ],
+  },
+  {
+    description: 'Aliquam id orci ut lectus varius viverra. Aliquam id orci ut lectus varius viverra.',
+    name: 'Geneva',
+    pictures: [],
+  },
+  {
+    description: '',
+    name: 'Prague',
+    pictures: [
+      {
+        src: 'http://picsum.photos/248/152?r=1',
+        description: 'In rutrum ac purus sit amet tempus. Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui.',
+      },
+      {
+        src: 'http://picsum.photos/248/152?r=5',
+        description: 'Cras aliquet varius magna, non porta ligula feugiat eget. Cras aliquet varius magna, non porta ligula feugiat eget.',
+      },
+      {
+        src: 'http://picsum.photos/248/152?r=3',
+        description: 'In rutrum ac purus sit amet tempus. Sed sed nisi sed augue convallis suscipit in sed felis. Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante. Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum.',
+      },
+      {
+        src: 'http://picsum.photos/248/152?r=2',
+        description: 'In rutrum ac purus sit amet tempus. Fusce tristique felis at fermentum pharetra. Cras aliquet varius magna, non porta ligula feugiat eget.',
+      },
+    ],
+  },
+  {
+    description: 'Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante. Cras aliquet varius magna, non porta ligula feugiat eget. In rutrum ac purus sit amet tempus.',
+    name: 'Berlin',
+    pictures: [
+      {
+        src: 'http://picsum.photos/248/152?r=5',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sed nisi sed augue convallis suscipit in sed felis. Sed sed nisi sed augue convallis suscipit in sed felis. Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      },
+      {
+        src: 'http://picsum.photos/248/152?r=4',
+        description: 'Aliquam id orci ut lectus varius viverra. Fusce tristique felis at fermentum pharetra. Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum. Fusce tristique felis at fermentum pharetra. Aliquam erat volutpat.',
+      },
+      {
+        src: 'http://picsum.photos/248/152?r=2',
+        description: 'Nunc fermentum tortor ac porta dapibus.',
+      },
+      {
+        src: 'http://picsum.photos/248/152?r=1',
+        description: 'Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum. In rutrum ac purus sit amet tempus.',
+      },
+    ],
+  },
+  {
+    description: '',
+    name: 'Stockholm',
+    pictures: [],
+  },
+];
 export const SortType = {
   DAY: 'sort-day',
   TIME: 'sort-time',
