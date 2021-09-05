ed8a14447dd72040190034c0f3478f0f7d0f46ad
Dmitry Vorobev <dm.siberian@gmail.com>
2021-09-05 23:15:41 +0300

    Added a draft of the StatsView component.

---
+++ src/view/stats.js
@@ -0,0 +1,285 @@
+import SmartView from './smart';
+
+import Chart from 'chart.js';
+import ChartDataLabels from 'chartjs-plugin-datalabels';
+
+const BAR_HEIGHT = 55;
+
+
+const createMoneyChartTemplate = (moneyCtx) => (
+  new Chart(moneyCtx, {
+    plugins: [ChartDataLabels],
+    type: 'horizontalBar',
+    data: {
+      labels: ['TAXI', 'BUS', 'TRAIN', 'SHIP', 'TRANSPORT', 'DRIVE'],
+      datasets: [{
+        data: [400, 300, 200, 160, 150, 100],
+        backgroundColor: '#ffffff',
+        hoverBackgroundColor: '#ffffff',
+        anchor: 'start',
+      }],
+    },
+    options: {
+      plugins: {
+        datalabels: {
+          font: {
+            size: 13,
+          },
+          color: '#000000',
+          anchor: 'end',
+          align: 'start',
+          formatter: (val) => `€ ${val}`,
+        },
+      },
+      title: {
+        display: true,
+        text: 'MONEY',
+        fontColor: '#000000',
+        fontSize: 23,
+        position: 'left',
+      },
+      scales: {
+        yAxes: [{
+          ticks: {
+            fontColor: '#000000',
+            padding: 5,
+            fontSize: 13,
+          },
+          gridLines: {
+            display: false,
+            drawBorder: false,
+          },
+          barThickness: 44,
+        }],
+        xAxes: [{
+          ticks: {
+            display: false,
+            beginAtZero: true,
+          },
+          gridLines: {
+            display: false,
+            drawBorder: false,
+          },
+          minBarLength: 50,
+        }],
+      },
+      legend: {
+        display: false,
+      },
+      tooltips: {
+        enabled: false,
+      },
+    },
+  }));
+
+
+const createTypeChartTemplate = (typeCtx) => (
+  new Chart(typeCtx, {
+    plugins: [ChartDataLabels],
+    type: 'horizontalBar',
+    data: {
+      labels: ['TAXI', 'BUS', 'TRAIN', 'SHIP', 'TRANSPORT', 'DRIVE'],
+      datasets: [{
+        data: [4, 3, 2, 1, 1, 1],
+        backgroundColor: '#ffffff',
+        hoverBackgroundColor: '#ffffff',
+        anchor: 'start',
+      }],
+    },
+    options: {
+      plugins: {
+        datalabels: {
+          font: {
+            size: 13,
+          },
+          color: '#000000',
+          anchor: 'end',
+          align: 'start',
+          formatter: (val) => `${val}x`,
+        },
+      },
+      title: {
+        display: true,
+        text: 'TYPE',
+        fontColor: '#000000',
+        fontSize: 23,
+        position: 'left',
+      },
+      scales: {
+        yAxes: [{
+          ticks: {
+            fontColor: '#000000',
+            padding: 5,
+            fontSize: 13,
+          },
+          gridLines: {
+            display: false,
+            drawBorder: false,
+          },
+          barThickness: 44,
+        }],
+        xAxes: [{
+          ticks: {
+            display: false,
+            beginAtZero: true,
+          },
+          gridLines: {
+            display: false,
+            drawBorder: false,
+          },
+          minBarLength: 50,
+        }],
+      },
+      legend: {
+        display: false,
+      },
+      tooltips: {
+        enabled: false,
+      },
+    },
+  }));
+
+
+const createTimeChartTemplate = (timeCtx) => (
+  new Chart(timeCtx, {
+    plugins: [ChartDataLabels],
+    type: 'horizontalBar',
+    data: {
+      labels: ['TAXI', 'BUS', 'TRAIN', 'SHIP', 'TRANSPORT', 'DRIVE'],
+      datasets: [{
+        data: [4, 3, 10, 4, 6, 8],
+        backgroundColor: '#ffffff',
+        hoverBackgroundColor: '#ffffff',
+        anchor: 'start',
+      }],
+    },
+    options: {
+      plugins: {
+        datalabels: {
+          font: {
+            size: 13,
+          },
+          color: '#000000',
+          anchor: 'end',
+          align: 'start',
+          formatter: (val) => `${val}H`,
+        },
+      },
+      title: {
+        display: true,
+        text: 'TIME-SPEND',
+        fontColor: '#000000',
+        fontSize: 23,
+        position: 'left',
+      },
+      scales: {
+        yAxes: [{
+          ticks: {
+            fontColor: '#000000',
+            padding: 5,
+            fontSize: 13,
+          },
+          gridLines: {
+            display: false,
+            drawBorder: false,
+          },
+          barThickness: 44,
+        }],
+        xAxes: [{
+          ticks: {
+            display: false,
+            beginAtZero: true,
+          },
+          gridLines: {
+            display: false,
+            drawBorder: false,
+          },
+          minBarLength: 50,
+        }],
+      },
+      legend: {
+        display: false,
+      },
+      tooltips: {
+        enabled: false,
+      },
+    },
+  }));
+
+
+const createStatsTemplate = () => (
+  `<section class="statistics">
+    <h2 class="visually-hidden">Trip statistics</h2>
+
+    <div class="statistics__item">
+      <canvas class="statistics__chart" id="money" width="900"></canvas>
+    </div>
+
+    <div class="statistics__item">
+      <canvas class="statistics__chart" id="type" width="900"></canvas>
+    </div>
+
+    <div class="statistics__item">
+      <canvas class="statistics__chart" id="time-spend" width="900"></canvas>
+    </div>
+  </section>`
+);
+
+export default class Stats extends SmartView {
+  constructor(points) {
+    super();
+
+    this._moneyChart = null;
+    this._typeChart = null;
+    this._timeChart = null;
+
+    this._points = points;
+
+    this._setCharts();
+  }
+
+  _setCharts() {
+    if (this._moneyChart !== null || this._typeChart !== null || this._timeChart !== null) {
+      this._moneyCart = null;
+      this._typeChart = null;
+      this._timeChart = null;
+    }
+
+    const moneyCtx = this.getElement().querySelector('#money');
+    const typeCtx = this.getElement().querySelector('#type');
+    const timeCtx = this.getElement().querySelector('#time-spend');
+
+    this._moneyChart = this._renderMoneyChart(moneyCtx, this._points);
+    this._typeChart = this._renderTypeChart(typeCtx, this._points);
+    this._timeChart = this._renderTimeChart(timeCtx, this._points);
+  }
+
+  removeElement() {
+    super.removeElement();
+
+    if (this._moneyChart !== null || this._typeChart !== null || this._timeChart !== null) {
+      this._moneyChart = null;
+      this._typeChart = null;
+      this._timeChart = null;
+    }
+  }
+
+  getTemplate() {
+    return createStatsTemplate();
+  }
+
+  _renderMoneyChart(moneyCtx, points) {
+    moneyCtx.height = BAR_HEIGHT * 5;
+    createMoneyChartTemplate(moneyCtx, points);
+  }
+
+  _renderTypeChart(typeCtx, points) {
+    typeCtx.height = BAR_HEIGHT * 5;
+    createTypeChartTemplate(typeCtx, points);
+  }
+
+  _renderTimeChart(timeCtx, points) {
+    timeCtx.height = BAR_HEIGHT * 5;
+    createTimeChartTemplate(timeCtx, points);
+  }
+}
