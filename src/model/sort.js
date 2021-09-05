9f2f233362b65735b854724f54f7c9b5db642e82
Dmitry Vorobev <dm.siberian@gmail.com>
2021-09-05 20:54:55 +0300

    Added Sort model.

---
+++ src/model/sort.js
@@ -0,0 +1,34 @@
+import AbstractObserver from './abstract-observer.js';
+import {SortType} from '../const.js';
+import {sortPointDay, sortPointTime, sortPointPrice} from '../utils/sort.js';
+
+
+export default class Sort extends AbstractObserver {
+  constructor(filterModel) {
+    super();
+    this._activeSortType = SortType.DAY;
+    this._filterModel = filterModel;
+  }
+
+  setSort(updateType, sortType) {
+    this._activeSortType = sortType;
+    this._notify(updateType, sortType);
+  }
+
+  getSort() {
+    return this._activeSortType;
+  }
+
+  getSortedPoints() {
+    const points = this._filterModel.getFilteredPoints();
+
+    switch (this.getSort()) {
+      case SortType.DAY:
+        return points.sort(sortPointDay);
+      case SortType.TIME:
+        return points.sort(sortPointTime);
+      case SortType.PRICE:
+        return points.sort(sortPointPrice);
+    }
+  }
+}
