b6bd55afc96ef3d0c10a5e3ef18edc85fea85385
Dmitry Vorobev <dm.siberian@gmail.com>
2021-09-13 16:56:09 +0300

    Added MINUTE_TO_MS constant.

--- src/view/abstract.js
+++ src/view/abstract.js
@@ -1,6 +1,7 @@
 import {createElement} from '../utils/render';
 
 const SHAKE_ANIMATION_TIMEOUT = 600;
+const MINUTE_TO_MS = 1000;
 
 
 export default class Abstract {
@@ -30,7 +31,7 @@ export default class Abstract {
   }
 
   shake(callback) {
-    this.getElement().style.animation = `shake ${SHAKE_ANIMATION_TIMEOUT / 1000}s`;
+    this.getElement().style.animation = `shake ${SHAKE_ANIMATION_TIMEOUT / MINUTE_TO_MS}s`;
     setTimeout(() => {
       this.getElement().style.animation = '';
       callback();
